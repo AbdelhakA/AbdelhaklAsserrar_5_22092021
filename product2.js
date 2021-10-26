5e56cb31d9bf071314138ce1534cad65152bfc6a
AbdelhakA <asserrar.abdelhak@gmail.com>
2021-10-26 19:43:43 +0200

    ajout HTML

--- product2.js
+++ product2.js
@@ -1,70 +1,80 @@
 (async function () {
-    const items = await getItem()
+    const items = await getItems()
   
     for (item of items) {
-        displayItem(item)
+       displayItem(item)
     }
-
+    
   })()
   
-  function getItem() {
+  function getItems() {
     return fetch("http://localhost:3000/api/products")
         .then(function (httpBodyResponse) {
             return httpBodyResponse.json()
         })
-        .then(function (item) {
-            return item
+        .then(function (items) {
+            return items
         })
         .catch(function (error) {
             alert(error)
         })
   }
-  
-  for (product of products) {
 
-    let productId = product._id; 
+  function displayItem (item) { 
+    let productId = item._id; 
     let price = document.getElementById('price');
     let description = document.getElementById('description');
-    let productPicLink = product.imageUrl
+    let productPicLink = item.imageUrl
     let image = document.getElementById('item_pic');
-    let colors = product.colors;
+    let colors = item.colors;
     let colorPlace = document.createElement('option');
-    console.log(colorPlace);
     let colorOption = document.getElementById('item_color');
+
+
     
     
      
+    let str = window.location.href;
+    let url = new URL(str); 
+    let id  = url.searchParams.get("id"); 
     
 
-    if (id === productId) {
+  if (productId === id) {
+     console.log(item);
+    // price.innerHTML = 
+    // description.innerHTML = product.description;
+    // image.setAttribute('src', '../images/' + productPicLink);
 
-      for (var i = 0 ; i < colors.length ; i++) {
-        colorOption.value = colors[i];
-        colorOption.innerText = colors[i];
-        
-        
-      }
+    `<div class="item__img" >
+              <img id="item_pic" src="${item.imageUrl}" alt="">
+            </div>
+            <div class="item__content">
 
-      for (var i = 0 ; i < colors.length ; i++) {
-        colorPlace.value = colors[i];
-        colorPlace.innerText = colors;
-        
-        
-      }
+              <div class="item__content__titlePrice">
+                <h1 id="title"><!-- Nom du produit --></h1>
+                <p>Prix : <span id="price">${item.price}</span>€</p>
+              </div>
 
-      // for (let option; option < colors.length; option++) {
-      //   colorPlace.value = colors[i];
-      //   colorPlace.innerText = colors[i];
-      //   console.log(colorPlace);
-      // }
+              <div class="item__content__description">
+                <p class="item__content__description__title">Description :</p>
+                <p id="description">${item.description}</p>
+              </div>
 
+              <div class="item__content__settings">
+                <div class="item__content__settings__color">
+                  <label for="color-select">Choisir une couleur :</label>
+                  <select name="color-select" id="colors">
+                      <option value="${item.colors}">${item.colors}</option>
+                      
+                      
+                  </select>
+                </div>
 
-
-
-      
-      price.innerHTML = product.price;
-      description.innerHTML = product.description;
-      image.setAttribute('src', '../images/' + productPicLink);
-    }
-    
+                <div class="item__content__settings__quantity">
+                  <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+                  <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+                </div>
+              </div>`
+            
+  }
 }    
